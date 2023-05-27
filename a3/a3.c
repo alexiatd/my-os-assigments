dc8351bbbb901a95b67229f8996dbe7890247210
alexiatd <teodora115@yahoo.com>
2023-05-28 02:53:11 +0300

    incercare

--- a3/a3.c
+++ a3/a3.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -24,23 +25,21 @@ struct section_header
 
 int main(int argc, char **argv)
 {
-    int n, fd[2], smh_fd;
-    int req,res;
-    // ssize_t byts;
+    int req, res;
+    ssize_t byts;
     program_name = argv[0];
-    // char *buf[10];
-    // char mess[250];
-    // void *shm_ptr;
+    char *buf, *buf1;
+    char text[4],shm_ptr;
 
-    if(mkfifo(RESP_PIPE_NAME, 0600)){
+    if (mkfifo(RESP_PIPE_NAME, 0666))
+    {
         printf("ERROR \ncannot create the response pipe");
         exit(1);
     }
 
-
     req = open(REQ_PIPE_NAME, O_RDONLY);
 
-    if (req ==-1)
+    if (req == -1)
     {
         printf("ERROR \ncannot open the request pipe");
         exit(2);
@@ -48,12 +47,12 @@ int main(int argc, char **argv)
 
     res = open(RESP_PIPE_NAME, O_WRONLY);
 
-    if (res ==-1)
+    if (res == -1)
     {
         printf("ERROR \ncannot open the response pipe");
         exit(3);
     }
-    char mess[5]="HELLO$";
+    char mess[6] = "HELLO$";
 
     if (write(res, mess, strlen(mess)) == -1)
     {
@@ -61,37 +60,66 @@ int main(int argc, char **argv)
         exit(4);
     }
     printf("SUCCESS");
-    /*
-        for (;;)
-        {
-            byts = read(fd[0], mess, sizeof(mess));
 
-            if (strcmp(byts, "PING$") == 0)
+    int size = 0, i=0;
+    char nr;
+    unsigned int n = 99110;
+
+    while (1)
+    {
+        //  while(read(req, buf, 1)){
+        //     if(strcmp(buf,'$')==0)
+
+        byts = read(req, buf, 250);
+
+/*
+        buf1=strtok(buf,"$");
+        i=0;
+        while(buf1!=0){
+            strcpy(text[i],buf1);
+            i++;
+            buf1=strtok(NULL,"$");
+        }
+*/
+        //  buf[6] = '\0';
+
+        if (strncmp(buf, "PING$", 5) == 0)
+        {
+            // printf("gfgf");
+            write(res, "PING$", 5);
+            write(res, "PONG$", 5);
+
+            // sprintf(nr, "%x", n);
+            write(res, &n, sizeof(unsigned int));
+        }
+        else if (strncmp(buf, "CREATE_SHM$",11) == 0)
+        {
+            
+            //if (strncmp(buf, "3262703", 7) == 0)
+               int smh_fd = shm_open("/iEPooM", O_CREAT | O_RDWR, 0664);
+            shm_ptr = mmap(NULL, 3262703, PROT_READ | PROT_WRITE, MAP_SHARED, smh_fd, 0);
+            if (smh_fd < 0 && ftruncate(smh_fd, 3262703) < 0)
             {
-                write(fd[0], "PING$PONG$99110", byts);
+                write(smh_fd, "CREATE_SHM$", byts);
+                write(smh_fd, "ERROR$", byts);
+                exit(2);
             }
-            else if (strcmp(byts, "CREATE_SHM$3262703$") == 0)
+            else
             {
-                smh_fd = shm_open("/iEPooM", O_CREAT | O_RDWR, 0664);
-                //shm_ptr = mmap(NULL, 3000000, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-                if (smh_fd < 0 && ftruncate(smh_fd, 3000000) < 0 )
-                {
-                    write(smh_fd, "CREATE_SHM$", byts);
-                    write(smh_fd, "ERROR$", byts);
-                    exit(2);
-                }
-                else
-                {
-                    write(smh_fd, "CREATE_SHM$", byts);
-                    write(smh_fd, "SUCCESS$", byts);
-                    exit(3);
-                }
+                write(smh_fd, "CREATE_SHM$", byts);
+                write(smh_fd, "SUCCESS$", byts);
+                exit(3);
             }
         }
-    */
+        else if (strncmp(buf, "EXIT$", 5) == 0)
+        {
+            exit(1);
+        }
+    }
+
     close(req);
     close(res);
-    unlink(res);
+    unlink(RESP_PIPE_NAME);
 
     return 0;
 }
